95db97d70c87b64e8f7a1b253751cc59a8e6fa80
pankaj jain <pj9009815223@gmail.com>
2024-03-23 22:59:44 +0530

    problem1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,49 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
-
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule,HttpClientModule,FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'test';
+  newUser: User = {
+    userName: '',
+    designation: '',
+    salary: 0,
+  };
+  users: any = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.fetchUsers();
+  }
+  // users : User[] = [];
+
+  onSubmit(newUser: any) {
+    this.users.push({ ...this.newUser });
+    this.http
+      .post('https://masaieval-default-rtdb.firebaseio.com/users.json', newUser)
+      .subscribe((res) => {
+        console.log(res, 'POST API');
+        //to make the form empty after submitting the data
+        this.newUser = { userName: '', designation: '', salary: 0 };
+      });
+  }
+
+  //fetching data realtime...>>>>
+
+  fetchUsers() {
+    this.http
+      .get('https://masaieval-default-rtdb.firebaseio.com/users.json')
+      .subscribe((res) => {
+        console.log(res);
+      });
+  }
 }
